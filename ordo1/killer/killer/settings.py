f12623aa220dd0119e45dd31df493eefd414f43f
fatmeme jadidi <fjadidi2001@gmail.com>
2023-03-22 23:40:59 -0400

    second edit

--- ordo1/killer/killer/settings.py
+++ ordo1/killer/killer/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'killer.apps.KillerConfig'
+    'killer.apps.KillerConfig',
 ]
 
 MIDDLEWARE = [
