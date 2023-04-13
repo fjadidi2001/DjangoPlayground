f77166ffff51e444a82b7574d5f5d710e22f0464
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-12 23:43:21 -0400

    added url

--- DAppWithPgSql/project2/myproject/urls.py
+++ DAppWithPgSql/project2/myproject/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from ..notes.views import NoteViewSet
+
+notes_router = routers.SimpleRouter()
+notes_router.register(
+    r'notes',
+    NoteViewSet,
+    basename='note',
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/', include(notes_router.urls))
 ]
