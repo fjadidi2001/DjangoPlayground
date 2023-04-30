4fc02a545bd831387f9ba81ad04e42d21df561d3
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-29 23:48:09 -0400

    added def to view

--- ordo1/Azfirst8/azfirst8app/views.py
+++ ordo1/Azfirst8/azfirst8app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def saturday(request):
+    return HttpResponse('this is saturday')
+
+
+def sunday(request):
+    return HttpResponse('this is sunday')
