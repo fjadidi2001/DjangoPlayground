b92466eada0ade158f8b9a6bce892d1b45d156ff
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-29 22:10:34 -0400

    added model

--- Store/StoreApp/models.py
+++ Store/StoreApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class StoreModel(models.Model):
+    Price = models.FloatField(max_length=50)
+    Discount = models.IntegerField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __float__(self):
+        return self.Price
+
