f51855a87fffd1feba75d5a232d3d0e087e05dd2
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-09 23:54:47 -0400

    fix main url

--- onlineShopping/mysite/mysite/urls.py
+++ onlineShopping/mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("shop/", include("shoppapp.urls")),
+    path("shop/", include("shopapp.urls")),
     path("admin/", admin.site.urls),
 ]
