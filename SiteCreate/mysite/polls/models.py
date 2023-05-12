717970484949a4187fb9180d717f3cf70cc86145
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-11 23:59:16 -0400

    fix models

--- SiteCreate/mysite/polls/models.py
+++ SiteCreate/mysite/polls/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 import datetime
 
 
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
