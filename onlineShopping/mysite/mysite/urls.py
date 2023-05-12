fb5cdc2833bcc84ec538baafc09fb961b32df48c
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-11 23:54:53 -0400

    change the app name

--- onlineShopping/mysite/mysite/urls.py
+++ onlineShopping/mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("shop/", include("shopapp.urls")),
+    path("shop/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
